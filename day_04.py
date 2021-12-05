cf0782059f9ce9de6f379b984ac94f77f76a20bc
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-12-05 00:04:38 -0800

    REFACTOR to remove condition and use splat operator

--- day_04.py
+++ day_04.py
@@ -5,11 +5,12 @@ Board = List[List[int]]
 def final_score(boards: List[Board], draws: List[int]) -> int:
     for num in draws:
         for board in boards:
-            row, col = mark(board, num)
-            if row >= 0 and col >= 0 and is_winning(board, row, col):
+            if is_winning(board, *mark(board, num)):
                 return num * unmarked_sum(board)
 
 def is_winning(board: Board, target_row: int, target_col: int) -> bool:
+    if target_row < 0 or target_col < 0:
+        return False
     rows, cols = len(board), len(board[0])
     is_winning_row = sum(board[target_row]) == -rows
     is_winning_col = sum([board[row][target_col] for row in range(rows)]) == -cols
@@ -28,5 +29,6 @@ def unmarked_sum(board: Board) -> int:
     summation = 0
     for row in board:
         for val in row:
-            if val > 0: summation += val
+            if val > 0:
+                summation += val
     return summation

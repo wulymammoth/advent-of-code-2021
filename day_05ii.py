b67027499c5a0d2b72a6a598b2069f12d6b8a4fe
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-12-06 16:54:13 -0800

    WIP day 5 // part 2

---
+++ day_05ii.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from functools import reduce
+from typing import List, Tuple
+
+Coordinate = Tuple[int, int]
+CoordPair  = Tuple[Coordinate, Coordinate]
+
+def overlapping_vents(vents: List[CoordPair]) -> int:
+    # NOTE: x is the col and y is the row (when thinking about boards)
+    counter = Counter()
+    for vent in vents:
+        (x1, y1), (x2, y2) = vent
+        x1, x2 = min(x1, x2), max(x1, x2)
+        y1, y2 = min(y1, y2), max(y1, y2)
+        if x1 == x2 or y1 == y2:
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    counter[(x, y)] += 1
+        else: # TODO: diagonal
+            pass
+    return reduce(lambda summation, x: summation + 1 if x > 1 else 0, counter.values(), 0)
---
+++ test_day05ii.py
@@ -0,0 +1,33 @@
+import unittest
+from day_05ii import overlapping_vents
+
+INPUT_PATH = './fixtures/day-05.txt'
+
+class TestDay05Part2(unittest.TestCase):
+    def setUp(self):
+        with open(INPUT_PATH, 'r') as vents:
+            self.vents = []
+            for line in vents:
+                pair1, pair2 = line.strip().split('->')
+                x1, y1 = [int(num) for num in pair1.strip().split(',')]
+                x2, y2 = [int(num) for num in pair2.strip().split(',')]
+                self.vents.append(((x1, y1), (x2, y2)))
+
+    def test_example(self):
+        vents = [
+            ((0,9), (5,9)),
+            ((8,0), (0,8)),
+            ((9,4), (3,4)),
+            ((2,2), (2,1)),
+            ((7,0), (7,4)),
+            ((6,4), (2,0)),
+            ((0,9), (2,9)),
+            ((3,4), (1,4)),
+            ((0,0), (8,8)),
+            ((5,5), (8,2))
+        ]
+        self.assertEqual(overlapping_vents(vents), 12)
+
+    @unittest.skip
+    def test_actual(self):
+        self.assertEqual(overlapping_vents(self.vents), 0)
